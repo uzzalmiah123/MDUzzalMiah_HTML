4e6791ba045151f3022fc1c5a076fa4d2ff2b4de
uzzalmiah123 <49460185+uzzalmiah123@users.noreply.github.com>
2020-09-30 23:12:32 -0400

    Add Interaction my self_portrait_v3

---
+++ self_portrait_v3/sketch.js
@@ -0,0 +1,106 @@
+/* 
+Self Portrait v1 
+By MD Uzzal Miah
+*/
+// Global scope
+
+var x = 460; // character origin
+var y = 150;
+
+var eyeSize = 100;
+var mouthSize = eyeSize;
+
+var faceRed = 0;
+var faceGreen = 180;
+var faceBlue = 140;
+
+function setup() {
+	createCanvas(650, 360);
+	x = width / 2;
+}
+
+function draw() {
+	background("#CD5C5C");
+	
+	faceRed = map(mouseX, 0, height, 0, 255);
+
+ 
+	// ear 
+	fill(218,159,83); // Left ear
+	arc(x - 110, y - 20, 60, 60, 4, PI + QUARTER_PI);
+     
+     //Right Ear
+	fill(218,159,83);
+	arc(x + 97, y - 20, 60, 60, 4, PI + QUARTER_PI);
+
+	//face
+	noStroke();
+	fill(faceRed,faceGreen,faceBlue); // Th
+	triangle(x - 160,y - 95, x + 140, y- 90, x, y + 200)
+
+	// eyes
+
+	// left eye
+	fill(220, 220, 220); // Gainsboro
+	circle(x - 60, y - 20, eyeSize);
+	fill(0); // Black
+	circle(x - 60, y - 20, eyeSize / 2);
+
+    //Right eay
+    fill(220, 220, 220); // Gainsboro
+	ellipse(x + 60, y - 20, eyeSize, eyeSize / 2);
+    fill(0); // Black
+	ellipse(x+ 60, y - 20, eyeSize / 1.7, eyeSize / 3.3);
+
+
+	
+	
+
+    // mouth
+    stroke("#D68910");
+    strokeWeight(10)
+    noFill();
+
+    var mouthHeight = map(mouseY, 0, height, mouthSize / 3.3, mouthSize);
+
+	rect(x - 50, y + 80, mouthSize, mouthHeight, mouthSize / 10);
+    
+    
+
+
+	// hair
+	stroke("Black");
+	strokeWeight(3);
+    line( x - 70, y - 100, x - 50, y - 80);
+	line( x - 60, y - 100, x - 40, y - 80);
+	line( x - 50, y - 100, x - 30, y - 80);
+	line( x - 40, y - 100, x - 20, y - 80);
+	line( x - 30, y - 100, x - 10, y - 80);
+	line( x - 20, y - 100, x, y - 80);
+	line( x - 10, y - 100, x + 10, y - 80);
+	line( x, y - 100, x + 20, y - 80);
+	line( x + 10, y - 100, x + 30, y - 80);
+	line( x + 20, y - 100, x + 40, y - 80);
+	line( x + 30, y - 100, x + 50, y - 80);
+	line( x + 40, y - 100, x + 60, y - 80);
+	line( x + 50, y - 100, x + 70, y - 80);
+
+
+
+
+	//Beard
+
+	line( x - 40, y + 70, x - 50, y + 80);
+	line( x - 30, y + 70, x - 40, y + 80);
+	line( x - 20, y + 70, x - 30, y + 80);
+	line( x - 10, y + 70, x - 20, y + 80);
+	line( x, y + 70,x - 10, y + 80);
+	line( x + 10, y + 70, x, y + 80);
+	line( x + 20, y + 70, x + 10, y + 80);
+	line( x + 30, y + 70, x + 20, y + 80);
+	line( x + 40, y + 70, x + 30, y + 80);
+	line( x + 50, y + 70, x + 40, y + 80);
+	line( x + 60, y + 70, x + 50, y + 80);
+
+
+}
