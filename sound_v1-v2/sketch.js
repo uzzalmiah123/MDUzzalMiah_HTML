d87af93769ac31a86a3a568708c5e89545639261
uzzalmiah123 <49460185+uzzalmiah123@users.noreply.github.com>
2020-10-28 03:22:10 -0400

    I almost finist my Sound v1

---
+++ sound_v1-v2/sketch.js
@@ -0,0 +1,138 @@
+/* sound V 1- 2. */
+
+var button;
+//Images
+var pianoImage;
+var dropwaterImage;
+var car1Image;
+var car2Image;
+var drumImage;
+var rainImage;
+var windyImage;
+var birdsImage;
+
+// Sound
+var waterdropSound;
+var pianoSound;
+var birdsSound;
+var horn1Sound;
+var horn2Sound;
+var rainSound;
+var drumSound;
+var windySound;
+
+function preload() {
+	// Images
+	pianoImage = loadImage('piano.jpg'); // Piano Space Key 32
+	dropwater = loadImage('dropwater.jpg'); // Dropwater ArrowUp Key 38
+	car1Image = loadImage('car1.jpg'); // Car 1 ArrowLeft key 37
+	car2Image = loadImage('car2.jpg'); // Car 2 ArrowRight key 39
+	rainImage = loadImage('rain.jpg'); // Rain Enter key 13
+	windyImage = loadImage('windy.jpg'); // Windy Shift key 16
+	birdsImage = loadImage('birds.jpg'); // Birds Tab key 9
+	drumImage = loadImage('drum.jpg'); // Durm ArrowDown key 40
+
+	// Sound
+	rainSound = loadSound('Rain.wav'); // Rain Enter key 13
+	windySound = loadSound('windy.mp3'); // Windy Shift key 16
+	birdsSound = loadSound('Bird.mp3'); // Birds Tab key 9
+	pianoSound = loadSound('Piano.mp3'); // Piano Space Key 32
+	waterdropSound = loadSound('waterdrop.wav'); // Dropwater ArrowUp Key 38
+	horn1Sound = loadSound('horn1.wav'); // Car 1 ArrowLeft key 37
+	horn2Sound = loadSound('horn2.wav'); // Car 2 ArrowRight key 39
+	drumSound = loadSound('durm.wav'); // Durm ArrowDown key 40
+
+}
+
+function setup() {
+	createCanvas(640, 360);
+	pianoSound.playMode('restart');
+	rainSound.playMode('restart');
+	windySound.playMode('restart');	
+
+	button = createButton("pause");
+	button.mousePressed(togglePlaying);
+}
+
+
+
+
+
+
+function draw(){
+	background('#7FFFD4');
+if (pianoSound.isPlaying()){
+    image(pianoImage,0, 0,width, height);
+}
+	if (waterdropSound.isPlaying()){
+		    image(dropwater,0, 0,width, height);
+
+	}
+	if (horn1Sound.isPlaying()){
+		    image(car1Image,0, 0,width, height);
+	 }
+	 
+	if (horn2Sound.isPlaying()){
+		    image(car2Image,0, 0,width, height);
+	 }
+	
+	 if (rainSound.isPlaying()){
+		    image(rainImage,0, 0,width, height);
+	 }
+	 
+	 if (windySound.isPlaying()){
+		    image(windyImage,0, 0,width, height);
+	 }
+	 
+	 if (birdsSound.isPlaying()){
+		    image(birdsImage,0, 0,width, height);
+	 }
+	 
+	 if (drumSound.isPlaying()){
+		    image(drumImage,0, 0,width, height);
+	 }
+
+}
+
+
+
+
+
+
+
+
+
+function keyPressed(){
+	if (keyCode == 32){ // Space Chaaracter
+		pianoSound.play();
+	}
+	if (keyCode == 38){ // ArrowUp
+		waterdropSound.play();
+	}
+	if (keyCode == 37){ // ArrowLeft
+		horn1Sound.play();
+	}
+	if (keyCode == 39){ // ArrowRight
+		horn2Sound.play();
+	}
+	if (keyCode == 13){ // Enter
+		rainSound.play();
+	}
+	if (keyCode == 16){ // Shift
+		windySound.play();
+	}
+	if (keyCode == 9){ // Tab
+		birdsSound.play();
+	} 
+	if (keyCode == 40){ // ArrowDown
+		drumSound.play();
+	}
+}
+
+function togglePlaying(){
+	birdsSound.pause();
+	windySound.pause();
+	rainSound.pause();
+	pianoSound.pause();
+}
+
