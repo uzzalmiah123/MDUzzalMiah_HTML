5e59f82746eb23078867cbf21c00570bfb21a661
uzzalmiah123 <49460185+uzzalmiah123@users.noreply.github.com>
2020-10-28 03:22:25 -0400

    o

---
+++ sound_v1-v1/sketch.js
@@ -0,0 +1,47 @@
+/* sound V 1. */
+
+var pressingSound;
+var pianoSound;
+var button;
+
+function preload() {
+	pressingSound = loadSound('pressing.mp3');
+	pianoSound = loadSound('piano.mp3')
+}
+
+function setup() {
+	createCanvas(640, 360);
+
+}
+
+function draw() {
+	background('#7FFFD4');
+
+	if (pianoSound.isPlaying()){
+  	background('red');
+  }
+  var currentTime = pianoSound.currentTime();
+  var duration = pianoSound.duration();
+  var timeElapsed = map(currentTime, 0, duration, 0, width);
+
+  fill('yellow');
+  noStroke();
+  rect(0, height - 100, timeElapsed, 100);
+
+ // volume
+ var vol = map(mouseY, 0, height, 1, 0);
+ pianoSound.setVolume(vol);
+fill('green');
+ rect(0, mouseY, 40, height,);
+
+
+}
+
+function mousePressed(){
+
+	if (pianoSound.isPlaying()){
+		pianoSound.pause();
+	}else {
+		pianoSound.play();
+	}
+}
